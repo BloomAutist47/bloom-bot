b9803ba4f7fb8e29110e94c8abc61254b07ea727
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-03-01 13:45:45 +0800

    Fixed encoding and get content issues causing it to raise an exception

--- Cogs/GoogleSearchCog.py
+++ Cogs/GoogleSearchCog.py
@@ -1,68 +1,52 @@
-import pyshorteners
-
 from .Base import *
 from discord.ext import commands
+from pprint import pprint
+from requests import get as requests_get
 
 class GoogleSearchCog(commands.Cog, BaseTools):
     def __init__(self, Bot):
         self.setup()
         self.bot = Bot
-        self.s = pyshorteners.Shortener()
 
 
-    def search(self, term, num_results=10, lang="en"):
+    async def search(self, term, num_results=10, lang="en"):
         usr_agent = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/61.0.3163.100 Safari/537.36'}
 
-        def fetch_results(search_term, number_results, language_code):
-            escaped_search_term = search_term.replace(' ', '+')
+        escaped_search_term = term.replace(' ', '+')
+        google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, num_results+1,
+                                                                              lang)
 
-            google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results+1,
-                                                                                  language_code)
-            # response = requests.get(google_url, headers=usr_agent)
-            response = self.get_site_content_looped(URL=google_url, headers=usr_agent)
-            # response = BaseProgram.loop.run_until_complete(self.get_site(google_url, headers=usr_agent))
-            # response.raise_for_status()
+        response = requests_get(google_url, headers=usr_agent)
+        links = {}
 
-            # return (response.decode("utf-8"), google_url)
-            return (response, google_url)
+        soup = Soup(response.content, 'html.parser')
+        result_block = soup.find_all('div', attrs={'class': 'g'})
+        for result in result_block:
+            link = result.find('a', href=True)
+            title = result.find('h3')
+            name = result.find("span").text
+            if link and title:
+                links[name] = link['href']
 
-        def parse_results(soup):
-            links = {}
-
-            # soup = Soup(raw_html, 'html.parser')
-            result_block = soup.find_all('div', attrs={'class': 'g'})
-            for result in result_block:
-                link = result.find('a', href=True)
-                title = result.find('h3')
-                name = result.find("span").text
-                if link and title:
-                    links[name] = link['href']
-            return links
-        html = fetch_results(term, num_results, lang)
-        return [parse_results(html[0]), html[1]]
-
+        return [links, google_url]
 
     @commands.command(pass_context=True)
     async def go(self, ctx, *, value:str=""):
         if not value:
             await ctx.send(self.embed_single("Google Search Warning", "Please input a search keyword."))
             return
-        item = self.search(value, 10)
-        # item = self.floop(lambda: self.search(value, 10))
-        link_all = self.floop(lambda: self.s.tinyurl.short(item[1]))
-        embedVar = discord.Embed(title=f"Search - __{value}__", color=self.block_color,
+        item = await self.search(value, 11)
+
+        embedVar = discord.Embed(title=f"Search: __{value}__", color=self.block_color,
+            url=item[1]
             )
         embedVar.set_author(name="Google Chrome", icon_url=BaseProgram.icon_google)
-        desc = f"[Click here for all Result]({link_all})\n\n**Results**\n"
-        # response = BaseProgram.loop.run_until_complete(
+        desc = f"**Results**\n"
         for i in item[0]:
-            # print(item[0][i])
-            # link_ = BaseProgram.loop.run_until_complete(BaseProgram.s.tinyurl.short(item[0][i]))
             link_ = item[0][i]
             desc += f"[{i}]({link_})\n"
         embedVar.description = desc
-        # embedVar.add_field(name="Results", value=desc, inline=False)
         await ctx.send(embed=embedVar)
         return
