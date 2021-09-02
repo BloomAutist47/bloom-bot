aed8717a141aa55965b09690b039492f32120cdb
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-09-02 11:42:34 +0800

    updated requirements

--- other/req.py
+++ other/req.py
@@ -1,96 +1,4 @@
-import numpy
-import os
-import tweepy
-
-import discord
-from discord.ext import commands
-from discord.ext import tasks
-from discord.ext.commands import CommandNotFound
-from discord.utils import get
-from discord import Intents
-
-from layeris.layer_image import LayerImage
-
-from layeris.utils.conversions import convert_float_to_uint
-from PIL import Image, ImageDraw
-
-import re
-import json
-import github3
-
-import requests
-from requests import get as requests_get
-from urllib.request import urlopen, Request
-import cloudscraper
-
-
-from urllib.request import urlopen, Request, urlretrieve
-from urllib.parse import urlencode
-from base64 import standard_b64encode
-
-from time import sleep
-from io import BytesIO
-
-
-from bs4 import BeautifulSoup as Soup
-
-import asyncio
-import aiosonic
-from aiosonic.timeout import Timeouts
-import aiohttp
-import nest_asyncio
-import html5lib
-
-
-from datetime import datetime, timedelta, date
-from dateutil.relativedelta import relativedelta
-from pytz import timezone
-
-
-from threading import Thread
-import dictdiffer  
-from random import randint
-from ast import literal_eval
-from pprint import pprint
-from dotenv import load_dotenv
-
-import re
-import os
-import requests
-import threading
-
-from pprintpp import pprint
-from datetime import datetime
-
-from io import BytesIO, StringIO
-from zipfile import ZipFile
-from pathlib import Path
-
-import xml.etree.ElementTree as ET
-
-from pprintpp import pprint
-from discord_webhook import DiscordWebhook, DiscordEmbed
-from datetime import datetime
-import os
-import textwrap
-import json
-import asyncpraw
-import asyncio
-
-import html2text
-
-from discord.ext import commands
-from ast import literal_eval
-from datetime import datetime
-from pprint import pprint
-from pytz import timezone
-import dictdiffer  
-from bs4 import BeautifulSoup as Soup
-from requests import get as requests_get
-from urllib.request import urlopen, Request
-import cloudscraper
-from time import sleep
-
+# base
 import os
 import re
 import json
@@ -104,3 +12,139 @@ import aiohttp
 import discord
 import nest_asyncio
 import html5lib
+
+from discord.ext import commands
+from bs4 import BeautifulSoup as Soup
+from dotenv import load_dotenv
+
+from pprint import pprint
+
+
+# BOAT SEARCH
+from discord.ext import commands
+from math import ceil
+
+# CHARACTER COG
+import re
+
+from discord.ext import commands
+from ast import literal_eval
+
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+from pytz import timezone
+
+
+# CLASS SEARCH
+import re
+from discord.ext import commands
+
+
+# GOOGLE
+from discord.ext import commands
+from pprint import pprint
+from requests import get as requests_get
+
+# GUIDE
+import os
+from discord.ext import commands
+
+# REDDIT
+from discord.ext import commands
+
+from pprintpp import pprint
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from datetime import datetime
+import os
+import textwrap
+import json
+import asyncpraw
+import asyncio
+
+# STREAM COG
+from discord.ext import commands
+from ast import literal_eval
+from datetime import datetime
+from pprint import pprint
+from pytz import timezone
+import dictdiffer  
+from bs4 import BeautifulSoup as Soup
+from requests import get as requests_get
+from urllib.request import urlopen, Request
+import cloudscraper
+from time import sleep
+
+# SWF PROCESSOR
+import re
+import os
+import requests
+import threading
+
+from pprintpp import pprint
+from datetime import datetime
+
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+from pathlib import Path
+
+from .Base import *
+from discord.ext import commands
+import xml.etree.ElementTree as ET
+
+
+
+# TEXTUPLOADER COG
+import re
+from discord.ext import commands, tasks
+from io import BytesIO
+import textwrap
+from pprintpp import pprint
+
+
+# TWITTER LISTENER
+import re
+import copy
+import asyncio
+import tweepy
+
+from discord.ext import commands, tasks
+
+from threading import Thread
+from pprintpp import pprint
+from discord.utils import get
+from datetime import datetime, timedelta
+
+from urllib.request import urlopen, Request, urlretrieve
+from urllib.parse import urlencode
+from base64 import standard_b64encode
+from json import loads
+
+
+# UTILS
+import os
+from discord.ext import commands, tasks
+from datetime import datetime
+from pytz import timezone
+
+
+# WIKI GOD
+import re
+
+from discord.ext import commands
+from pprintpp import pprint
+import html2text
+import textwrap
+
+
+
+
+
+
+
+
+
+
+
+
+
+
